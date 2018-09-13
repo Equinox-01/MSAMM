f2e31f6ad265ad138c36952ea538fbe92c6a78a0
Equinox-01 <vereskovskystas@gmail.com>
2018-09-14 01:03:28 +0300

    Add histogram and all work with front-end

--- Lab1/controllers/ApplicationController.rb
+++ Lab1/controllers/ApplicationController.rb
@@ -6,4 +6,22 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/' do
+    # Enter input data
+    a = params[:a]
+    r0 = params[:r0]
+    m0 = params[:m0]
+    # Proccessing module
+    
+    # Output data
+    @returnings_value = []  # Array.new(21) { rand(100...3000) }
+    @math_value = 0
+    @dispersion = 0
+    @standard_deviation = 0
+    @indirect_indications = 0
+    @period_length = 0
+    @aperiodic_section_length = 0
+    erb :result
+  end
 end
