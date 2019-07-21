0514a90663062513d436654b54f0d4a489dd501c
Stas Vereskovsky <vereskovskystas@gmail.com>
2019-07-22 00:05:53 +0300

    Refactor code

--- Lab1/algorithm/chart.rb
+++ Lab1/algorithm/chart.rb
@@ -1,29 +1,34 @@
 module Algorithm
   class Chart
     INTERVALS = 20
-    def self.build_chart(sequence)
-      intervals = Array.new(INTERVALS)
-      length = sequence.count
-      min = sequence.min
+
+    def initialize(sequence)
+      @intervals = Array.new(INTERVALS)
+      @length = sequence.count
+      @min = sequence.min
       range = sequence.max - sequence.min
-      delta = range.to_f / INTERVALS
-      intervals[0] = delta
-      (1..INTERVALS).each { |index| intervals[index] = delta + intervals[index - 1] }
-      numOfHits = Array.new(INTERVALS).fill(0)
-      (0...length).each do |index|
+      @delta = range.to_f / INTERVALS
+      @sequence = sequence
+    end
+
+    def build_chart
+      @intervals[0] = @delta
+      (1..INTERVALS).each { |index| @intervals[index] = @delta + @intervals[index - 1] }
+      num_of_hits = Array.new(INTERVALS).fill(0)
+      (0...@length).each do |index|
         INTERVALS.times.each do |sec_index|
-          tmp = min + sec_index * delta
-          if (sequence[index] >= tmp) && (sequence[index] <= tmp + delta)
-            numOfHits[sec_index] += 1
+          tmp = @min + sec_index * @delta
+          if (@sequence[index] >= tmp) && (@sequence[index] <= tmp + @delta)
+            num_of_hits[sec_index] += 1
             break
           end
         end
       end
 
       frequencies = Array.new(INTERVALS)
-      INTERVALS.times.each { |index| frequencies[index] = numOfHits[index].to_f / 1000000.to_f }
+      INTERVALS.times.each { |index| frequencies[index] = num_of_hits[index].to_f / 1_000_000 }
       result = {}
-      INTERVALS.times.each { |index| result.merge!(((min + delta * (index + 1)).round(3)) => frequencies[index]) }
+      INTERVALS.times.each { |index| result[(@min + @delta * (index + 1)).round(3)] = frequencies[index] }
       result
     end
   end
--- Lab1/spec/algorithm_specs/stats_spec.rb
+++ Lab1/spec/algorithm_specs/stats_spec.rb
@@ -13,22 +13,22 @@ RSpec.describe Algorithm::Stats do
 
   context 'check statistic' do
     it 'check math_value' do
-      expect(subject.instance_variable_get(:@expected)).to eq(0.5)
+      expect(subject.expected).to eq(0.5)
     end
     it 'check dispersion' do
-      expect(subject.instance_variable_get(:@dispersion).round(4)).to eq(0.05)
+      expect(subject.dispersion.round(4)).to eq(0.05)
     end
     it 'check standard_deviation' do
-      expect(subject.instance_variable_get(:@standardDeviation).round(4)).to eq(0.2236)
+      expect(subject.standard_deviation.round(4)).to eq(0.2236)
     end
     it 'check indirect_indications' do
-      expect(subject.instance_variable_get(:@indirectEvaluation)).to eq(1.0)
+      expect(subject.indirect_evaluation).to eq(0.5)
     end
     it 'check period_length' do
-      expect(subject.instance_variable_get(:@period_length)).to eq(4)
+      expect(subject.period_length).to eq(4)
     end
     it 'check aperiodic_section_length' do
-      expect(subject.instance_variable_get(:@aperiodicity)).to eq(4)
+      expect(subject.aperiodicity).to eq(4)
     end
   end
 end
