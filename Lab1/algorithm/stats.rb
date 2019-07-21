c96741003923f8c3fd31707bf082cbab7e730fdb
Stas Vereskovsky <vereskovskystas@gmail.com>
2019-07-22 00:05:53 +0300

    Refactor code

--- Lab1/algorithm/stats.rb
+++ Lab1/algorithm/stats.rb
@@ -1,56 +1,63 @@
 module Algorithm
   class Stats
+    attr_reader :expected, :dispersion, :standard_deviation,
+                :indirect_evaluation, :period_length, :aperiodicity
+
     MEASURE_ROUNDING = 6
+
     def initialize(sequence)
       @sequence = sequence
-      sum = sequence.sum
-      @expected = sum.to_f / @sequence.count
-      sum = 0
-      sequence.each_with_index {|tmp_seq, index| sum += (sequence[index] - @expected) * (sequence[index] - @expected) }
-      @dispersion = sum.to_f / sequence.count
-      @standardDeviation = Math.sqrt(@dispersion)
-      @indirectEvaluation = 2 * amount_of_pairs_for_indirectevaluation.to_f / sequence.count
-      @period_length = length_of_period
-      @aperiodicity = length_of_aperiodism(@period_length)
+      execute
     end
-    # Take all values to methods & memoized
+
+    private
+
+    def execute
+      sum = @sequence.sum
+      @expected = sum.to_f / @sequence.count
+      sum_dispersion = 0
+      @sequence.count.times.each do |index|
+        sum_dispersion += (@sequence[index] - @expected) * (@sequence[index] - @expected)
+      end
+      @dispersion = sum_dispersion.to_f / @sequence.count
+      @standard_deviation = Math.sqrt(@dispersion)
+      @indirect_evaluation = amount_of_pairs_for_indirectevaluation.to_f / @sequence.count
+      @period_length = length_of_period
+      @aperiodicity = length_of_aperiodism
+    end
 
     def amount_of_pairs_for_indirectevaluation
-      amount = 0
-      i = 0
-      while (i < @sequence.length)
-        amount += 1 if ((@sequence[i].to_f * @sequence[i].to_f + @sequence[i + 1].to_f * @sequence[i + 1].to_f).to_f < 1)
-        i = i + 2
+      amount = index = 0
+      while index < @sequence.count
+        amount += 1 if (@sequence[index]**2 + @sequence[index + 1]**2).to_f < 1
+        index += 2
       end
       amount
     end
 
     def length_of_period
-      v = @sequence[@sequence.length - 1]
-      isStart = false
-      i1 = 0
-      (0..@sequence.length).each do |index|
+      v = @sequence[-1]
+      is_start = false
+      tmp = 0
+      @sequence.count.times.each do |index|
         if @sequence[index] == v
-          if (!isStart)
-            i1 = index
-            isStart = true
+          if !is_start
+            tmp = index
+            is_start = true
           else
-            return index - i1
+            return index - tmp
           end
         end
       end
-      return 0
+      0
     end
 
-    def length_of_aperiodism(period)
+    def length_of_aperiodism
       @sequence.each_with_index do |tmp_seq, index|
-        if (index + @period_length < @sequence.count)
-          if (tmp_seq == @sequence[index + @period_length])
-            return index + @period_length
-          end
-        end
+        return index + @period_length if (index + @period_length < @sequence.count) &&
+                                         (tmp_seq == @sequence[index + @period_length])
       end
-      return 0
+      0
     end
   end
 end
--- Lab1/controllers/application_controller.rb
+++ Lab1/controllers/application_controller.rb
@@ -13,22 +13,20 @@ class ApplicationController < Sinatra::Base
     a = params[:a].to_i
     r0 = params[:R0].to_i
     m0 = params[:m0].to_i
-    sequence = Algorithm::Lehmer.new(a, m0, r0).sequence
-    graph_hash = Algorithm::Chart.build_chart(sequence)
-    @steps_histogram = []
-    @values_histogram = []
-    graph_hash.each_with_index { |key, value| @steps_histogram << key; @values_histogram << value }
 
+    sequence = Algorithm::Lehmer.new(a, m0, r0).sequence
+
+    graph_hash = Algorithm::Chart.new(sequence).build_chart
     @steps_histogram = graph_hash.keys
     @values_histogram = graph_hash.values
 
     stats = Algorithm::Stats.new(sequence)
-    @math_value = stats.instance_variable_get(:@expected)
-    @dispersion = stats.instance_variable_get(:@dispersion)
-    @standard_deviation = stats.instance_variable_get(:@standardDeviation)
-    @indirect_indications = stats.instance_variable_get(:@indirectEvaluation)
-    @period_length = stats.instance_variable_get(:@period_length)
-    @aperiodic_section_length = stats.instance_variable_get(:@aperiodicity)
+    @math_value = stats.expected
+    @dispersion = stats.dispersion
+    @standard_deviation = stats.standard_deviation
+    @indirect_indications = stats.indirect_evaluation
+    @period_length = stats.period_length
+    @aperiodic_section_length = stats.aperiodicity
 
     erb :result
   end
